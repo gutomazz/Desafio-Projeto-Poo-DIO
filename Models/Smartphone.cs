1b6e8b3557009a1117bcfeab3f901be52c13e2ea
Gustavo Thomaz <gutomazz@gmail.com>
2023-12-27 18:37:54 -0300

    subindo projeto já finalizado.

---
+++ Models/Smartphone.cs
@@ -0,0 +1,33 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+
+        // Propiedades implementadas de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string iMEI, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+            // Parametros dos construtores foram passados para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program {
+    public static void Main(){
+        Iphone i1 = new("81988888", "1008524c5xx", "iphone 12", 128);
+        i1.Ligar();
+        i1.ReceberLigacao();
+        i1.InstalarAplicativo("zapzap");
+
+        Nokia a1 = new("8198544657", "1554c558x4v", "Nokia a1", 64);
+        a1.Ligar();
+        a1.ReceberLigacao();
+        a1.InstalarAplicativo("Jogo do tigrinho");
+
+    }
+}
